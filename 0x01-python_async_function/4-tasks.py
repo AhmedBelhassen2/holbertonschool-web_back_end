2d79f4af782303f0a01d16e04204de4908e3ca7b
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-03-04 14:59:27 +0100

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -1,15 +1,16 @@
 #!/usr/bin/env python3
-"""
-    Take the code from wait_n and alter it into a new function task_wait_n
-"""
-from typing import List
+"""asqdsqdsqdsqdsqd"""
+
+
 import asyncio
+from typing import List
+
+wait_random = __import__('0-basic_async_syntax').wait_random
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
-    """
-        task_wait_n function
-    """
-    array = [task_wait_random(max_delay) for i in range(n)]
-    return [await rtat for rtat in asyncio.as_completed(array)]
+    """asqdsqdsqdsqdsqd"""
+    coroutines = (task_wait_random(max_delay) for i in range(n))
+    lst = await asyncio.gather(*coroutines)
+    return sorted(lst)
