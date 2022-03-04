d5328309bf1c8bc768d7cc1c50b6771ceb7f71bf
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-03-04 13:55:15 +0100

    4-tasks.py

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -3,6 +3,7 @@
 """tasks"""
 
 import asyncio
+import random
 from typing import List
 
 task_wait_random = __import__('3-tasks').task_wait_random
@@ -13,12 +14,12 @@ async def task_wait_n(n: int, max_delay: int) -> List[float]:
     Test file for printing
     the correct output of
     the task_wait_n coroutine"""
-    queue = [
+    f = [
         task_wait_random(max_delay) for i in range(n)
     ]
 
-    array = []
-    for j in asyncio.as_completed(queue):
-        rtat = await j
-        array.append(rtat)
-    return array
+    rs = []
+    for x in asyncio.as_completed(f):
+        r = await x
+        rs.append(r)
+    return rs
