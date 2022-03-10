f62fb96ced31f9ce62f3fb0496a160c89425e525
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-03-10 15:28:22 +0100

    0-async_generator.py

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -3,7 +3,6 @@
     coroutine that is repeated 10 times, each time it waits asynchronously
     for 1 second, then produces a random number between 0 and 10
 """
-import asyncio
 import random
 from asyncio import sleep
 from typing import Generator
