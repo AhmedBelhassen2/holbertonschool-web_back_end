944e32bfebc84787a2e31a575b7a143e40d0c44a
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-08-08 18:18:25 +0100

    exercice2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 """ The open source, in-memory data store used by millions of developers as a database, cache, streaming engine, and message broker. """
-from typing import Union, Callable, Optional, Any
+from typing import Union, Callable, Optional
 import redis
 import uuid
 import sys
 from functools import wraps
+
+def count_calls(f: Callable) -> Callable:
+        """
+        Create and return function that increments the count
+        for that key every time the method is called and returns
+        the value returned by the original method.
+        """
+        key = f.__qualname__
+        @wraps(f)
+        def wrapper(self, *args, **kwds):
+            """
+            wrapper function
+            """
+            self._redis.incr(key)
+            return f(self, *args, **kwds)
+        return wrapper  
 
 class Cache:
     """ class """
@@ -12,7 +28,7 @@ class Cache:
         """ Create a Cache class. In the __init__ method, store an instance of the Redis client as a private variable named _redis (using redis.Redis()) and flush the instance using flushdb. """
         self._redis = redis.Redis()
         self._redis.flushdb()
-
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """ method that takes a data argument and returns a string """
         key = str(uuid.uuid4())
@@ -32,18 +48,4 @@ class Cache:
         """ Converts bytes to int. """
         return int.from_bytes(data, sys.byteorder)
 
-    def count_calls(f: Callable) -> Callable:
-        """
-        Create and return function that increments the count
-        for that key every time the method is called and returns
-        the value returned by the original method.
-        """
-        key = f.__qualname__
-        @wraps(f)
-        def wrapper(self, *args, **kwds):
-            """
-            wrapper function
-            """
-            self._redis.incr(key)
-            return f(self, *args, **kwds)
-        return wrapper  
+    
