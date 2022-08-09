2d2d09277c978232305511473d09293445cc3827
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-08-09 14:16:14 +0100

    exercice2

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
-"""
-Redis basic module
-"""
+""" The open source, in-memory data store used
+by millions of developers as a database, cache,
+streaming engine, and message broker. """
 from typing import Union, Callable, Optional
 import redis
 import uuid
@@ -10,54 +10,53 @@ from functools import wraps
 
 
 def count_calls(method: Callable) -> Callable:
-    """ Calls counter decorator """
+    """
+        Create and return function that increments the count
+        for that key every time the method is called and returns
+        the value returned by the original method.
+        """
     key = method.__qualname__
+
     @wraps(method)
     def wrapper(self, *args, **kwds):
-        """Incrementation method's wrapper"""
+        """
+        wrapper function
+        """
         self._redis.incr(key)
         return method(self, *args, **kwds)
     return wrapper
 
 
-
 class Cache:
-    """ Cache class
-    """
+
+    """ class """
 
     def __init__(self):
-        """ __init __
-        """
+        """ Create a Cache class. In the __init__ method,
+        store an instance of the Redis client as a private
+        variable named _redis (using redis.Redis())
+        and flush the instance using flushdb. """
         self._redis = redis.Redis()
         self._redis.flushdb()
 
     @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
-        """
-        Takes a data argument and returns a string. The method
-        should generate a random key (e.g. using uuid), store the input
-        data in Redis using the random key and return the key.
-        """
-        key = str(uuid.uuid1())
+        """ method that takes a data argument and returns a string """
+        key = str(uuid.uuid4())
         self._redis.set(key, data)
         return key
 
-    def get(self,
-            key: str,
-            fn: Optional[Callable] = None) -> Union[str, bytes, int, float]:
-        """  Reading from Redis and recovering original type
-        """
-        res = self._redis.get(key)
-        return fn(res) if fn else res
+    def get(self, key: str, fn:
+            Optional[Callable] = None) -> Union[str, bytes, int, float]:
+        """  This callable will be used to convert
+        the data back to the desired format. """
+        rds = self._redis.get(key)
+        return fn(rds) if fn else rds
 
     def get_str(self, data: bytes) -> str:
-        """
-        Converts bytes to string.
-        """
+        """ Converts bytes to string. """
         return data.decode('utf-8')
 
     def get_int(self, data: bytes) -> int:
-        """
-        Converts bytes to int.
-        """
+        """ Converts bytes to int. """
         return int.from_bytes(data, sys.byteorder)
