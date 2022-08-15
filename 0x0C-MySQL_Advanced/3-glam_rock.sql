098fc362885a2f7a609b1454bdc64843ffe1ba7c
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-08-15 18:14:41 +0100

    3-glam_rock.sql

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,8 +1,7 @@
--- dummy comment dummy comment dummy comment
--- dummy comment dummy comment dummy comment
-CREATE DATABASE IF NOT EXISTS holberton;
--- all bands with Glam rock 
-SELECT band_name, (IFNULL(split, 2020) - formed) AS lifespan
+-- Old school band
+-- SQL script that lists all bands with Glam rock as their main style
+SELECT
+band_name, ifnull(split, 2020) - ifnull(formed, 0) AS lifespan
 FROM metal_bands
 WHERE style LIKE "%Glam rock%"
 ORDER BY lifespan DESC;
