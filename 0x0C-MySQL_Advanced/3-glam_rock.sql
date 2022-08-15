b90624e5d8535378bd9fce7445e5917e8ec3a8ac
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-08-15 17:03:04 +0100

    3-glam_rock.sql

--- 0x0C-MySQL_Advanced/3-glam_rock.sql
+++ 0x0C-MySQL_Advanced/3-glam_rock.sql
@@ -1,4 +1,4 @@
--- Glam rock
+-- Best band ever!
 SELECT band_name, IFNULL(split, 2020) - IFNULL(formed, 0) AS lifespan 
 FROM metal_bands WHERE FIND_IN_SET('Glam rock', style)
 ORDER BY lifespan DESC;
