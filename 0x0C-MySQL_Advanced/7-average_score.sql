b0f9d6acf2cee9048068391c62d57cf2151cd18a
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-08-16 15:53:11 +0100

    7-average_score.sql

--- 0x0C-MySQL_Advanced/7-average_score.sql
+++ 0x0C-MySQL_Advanced/7-average_score.sql
@@ -1,7 +1,8 @@
 -- Write a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
 DELIMITER $$
-CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+CREATE PROCEDURE ComputeAverageScoreForUser (IN user_id INT)
 BEGIN
-    END IF;
-    INSERT INTO corrections (user_id) VALUES (user_id, (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
+    UPDATE users
+    SET corrections (user_id) VALUES (user_id, (SELECT AVG(score) FROM corrections WHERE corrections.user_id = user_id);
+    WHERE id = user_id;
 END$$
