4d755d2fa3014e72b825ac9a60869421eaaf2881
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-08-16 15:39:11 +0100

    6-bonus.sql

--- 0x0C-MySQL_Advanced/6-bonus.sql
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -1,8 +1,5 @@
--- user_id, a users.id value (you can assume user_id is linked to an existing users)
--- project_name, a new or already exists projects - if no projects.name found in the table, you should create it
--- score, the score value for the correction
+-- Write a SQL script that creates a stored procedure AddBonus that adds a new correction for a student.
 DELIMITER $$
-DROP PROCEDURE AddBonus IF EXISTS Add_bonus;
 CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
 BEGIN
     IF NOT EXISTS (SELECT name FROM projects WHERE name = project_name)
