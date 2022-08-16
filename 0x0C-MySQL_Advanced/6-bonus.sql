e32cc536fed118a13fe717bb17374cbf80e70f7e
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-08-16 15:36:09 +0100

    6-bonus.sql

---
+++ 0x0C-MySQL_Advanced/6-bonus.sql
@@ -0,0 +1,13 @@
+-- user_id, a users.id value (you can assume user_id is linked to an existing users)
+-- project_name, a new or already exists projects - if no projects.name found in the table, you should create it
+-- score, the score value for the correction
+DELIMITER $$
+DROP PROCEDURE AddBonus IF EXISTS Add_bonus;
+CREATE PROCEDURE AddBonus (IN user_id INT, IN project_name VARCHAR(255), IN score INT)
+BEGIN
+    IF NOT EXISTS (SELECT name FROM projects WHERE name = project_name)
+    THEN
+    INSERT INTO projects (name) VALUES (project_name);
+    END IF;
+    INSERT INTO corrections (user_id, project_id, score) VALUES (user_id, (SELECT id FROM projects WHERE name = project_name LIMIT 1), score);
+END$$
