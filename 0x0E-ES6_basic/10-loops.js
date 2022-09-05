06a9895a082881737d5e854e0bdd6078acd106d0
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-09-05 19:14:50 +0100

    10-loops.js

--- 0x0E-ES6_basic/10-loops.js
+++ 0x0E-ES6_basic/10-loops.js
@@ -1,9 +1,9 @@
 /* eslint-disable */
 export default function appendToEachArrayValue(array, appendString) {
-    for (const idx of array) {
-      const value = array[idx];
-      array[idx] = appendString + value;
-    }
+  for (const idx of array) {
+    const value = array[idx];
+    array[idx] = appendString + value;
+  }
   
     return array;
   }
