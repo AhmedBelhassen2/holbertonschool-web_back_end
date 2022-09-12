b761c6c07d0fe71154880108792040f9eabdfc2c
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-09-12 15:32:32 +0100

    3-all.js

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,6 +1,6 @@
 import { createUser, uploadPhoto } from './utils';
 export default function handleProfileSignup() {
-    return Promise.all([uploadPhoto(), createUser()])
+  return Promise.all([uploadPhoto(), createUser()])
     .then((success) => { console.log(`${success[0].body} ${success[1].firstName} ${success[1].lastName}`); })
     .catch(() => { console.log('Signup system offline'); });
 }
