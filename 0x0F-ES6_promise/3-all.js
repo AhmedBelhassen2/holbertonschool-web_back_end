205cf86aa5d454f7cf5689439937083d60f932f8
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-09-12 15:40:17 +0100

    3-all.js

--- 0x0F-ES6_promise/3-all.js
+++ 0x0F-ES6_promise/3-all.js
@@ -1,4 +1,5 @@
 import { createUser, uploadPhoto } from './utils';
+
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((success) => { console.log(`${success[0].body} ${success[1].firstName} ${success[1].lastName}`); })
