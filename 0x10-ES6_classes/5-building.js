af659a5a0d5f732ab332cf4df58ff2189b8360ea
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-09-14 13:33:03 +0100

    5-building.js

---
+++ 0x10-ES6_classes/5-building.js
@@ -0,0 +1,12 @@
+export default class Building {
+  constructor(sqft) {
+    this._sqft = sqft;
+    if (this.constructor !== Building) {
+      if (!this.evacuationWarningMessage) { throw Error('Class extending Building must override evacuationWarningMessage'); }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
