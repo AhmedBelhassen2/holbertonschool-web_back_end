1ad599c2617247cdf27f0e83bd7549b4abf7ceaa
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-09-14 12:23:58 +0100

    3-currency.js

--- 0x10-ES6_classes/3-currency.js
+++ 0x10-ES6_classes/3-currency.js
@@ -1,23 +1,23 @@
 export default class Currency {
-  constructor(name, code) {
+  constructor(code, name) {
     this.name = name;
     this.code = code;
   }
 
-  set name(name) {
-    this._name = name;
-  }
-
-  set code(code) {
-    this._code = code;
+  get code() {
+    return this._code;
   }
 
   get name() {
     return this._name;
   }
 
-  get code() {
-    return this._code;
+  set code(code) {
+    this._code = code;
+  }
+
+  set name(name) {
+    this._name = name;
   }
 
   displayFullCurrency() {
