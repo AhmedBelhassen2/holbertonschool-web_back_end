220d96cc6048b4b92a72d4e1598cce37f128f37d
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-11-04 11:37:23 +0100

    3-payment.test.js

--- 0x13-unittests_in_js/3-payment.test.js
+++ 0x13-unittests_in_js/3-payment.test.js
@@ -6,15 +6,11 @@ const sendPaymentRequestToApi = require('./3-payment.js');
 
 describe('sendPaymentRequestToApi function', () => {
   const spyUtils = sinon.spy(Utils, 'calculateNumber');
-  const spyConsole = sinon.spy(console, 'log');
 
-
-  it('has the same math', () => {
+  it('validate the usage of the Utils function', () => {
     sendPaymentRequestToApi(100, 20);
-    expect(spyUtils.calledOnceWithExactly('SUM', 100, 20)).to.be.true;
-    expect(spyConsole.calledOnceWithExactly('The total is: 120')).to.be.true;
-
-    spyUtils.restore();
-    spyConsole.restore();  
-});
+    expect(spyUtils.calledOnce).to.be.true;
+    expect(spyUtils.calledWith('SUM', 100, 20)).to.be.true;
+    spyUtils.restore()
+  });
 });
