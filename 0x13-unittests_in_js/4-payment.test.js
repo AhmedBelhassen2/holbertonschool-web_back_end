a65b96b294207426ff583a04bfa02aaf8c75dc9f
ahmed77-jpg <ahmedbelhassen69@gmail.com>
2022-11-04 11:46:26 +0100

    4-payment.test.js

--- 0x13-unittests_in_js/4-payment.test.js
+++ 0x13-unittests_in_js/4-payment.test.js
@@ -7,12 +7,10 @@ const sendPaymentRequestToApi = require('./4-payment.js');
 describe('sendPaymentRequestToApi function', () => {
     it('validate the usage of the Utils function', () => {
 
-        const stubUtils = sinon.stub(Utils, 'calculateNumber');
-        const spyConsole = sinon.spy(console, 'log');
+    const stubUtils = sinon.stub(Utils, 'calculateNumber');stubUtils.returns(10);
+    const spyConsole = sinon.spy(console, 'log');
     
         sendPaymentRequestToApi(100, 20);
-    stubUtils.withArgs('SUM', 100, 20).returns(10);
-    sendPaymentRequestToApi(100, 20);
     expect(spyUtils.calledOnce).to.be.true;
     expect(spyUtils.calledWith('SUM', 100, 20)).to.be.true;
     spyUtils.restore()
